a1f45489ecf39b9540e230f6c477acf548236d45
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-03-25 00:24:24 -0600

    Code to get the current task list in the crontab

--- SchedulingTasks/SchedulingTasks/Crontab.cs
+++ SchedulingTasks/SchedulingTasks/Crontab.cs
@@ -1,7 +1,32 @@
-﻿namespace SchedulingTasks;
+﻿using System.Diagnostics;
+
+namespace SchedulingTasks;
 
 public static class Crontab
 {
+    /// <summary>
+    /// Code to get the current task list in the crontab
+    /// </summary>
+    public static string GetCrontab()
+    {
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = "/bin/bash",
+            Arguments = "-c \"crontab -l\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false
+        };
 
+        var process = new Process
+        {
+            StartInfo = processStartInfo
+        };
+
+        process.Start();
+        string result = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        return result;
+    }
 }
 
