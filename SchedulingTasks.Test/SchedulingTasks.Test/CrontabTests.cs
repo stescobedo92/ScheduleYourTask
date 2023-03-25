fc3662de1d7cc04dd5606f1e50775434da107471
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-03-25 00:48:45 -0600

    Added testing project

---
+++ SchedulingTasks.Test/SchedulingTasks.Test/CrontabTests.cs
@@ -0,0 +1,15 @@
+﻿namespace SchedulingTasks.Test;
+
+public class CrontabTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
