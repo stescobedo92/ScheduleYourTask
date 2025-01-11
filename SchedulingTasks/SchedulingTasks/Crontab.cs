10eab885532490c8abcd8b2c453ea15f4eefb1a8
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2025-01-11 17:55:44 -0600

    feat: Add parameter validation and exception handling to Crontab methods
    - Added validation for null or empty parameters in all methods.
    - Implemented exception handling to provide more informative error messages.
    - Ensured proper disposal of Process objects using 'using' statements.

--- SchedulingTasks/SchedulingTasks/Crontab.cs
+++ SchedulingTasks/SchedulingTasks/Crontab.cs
@@ -1,106 +1,224 @@
 ﻿using System.Diagnostics;
 
-namespace SchedulingTasks;
-
-public static class Crontab
+namespace SchedulingTasks
 {
-    /// <summary>
-    /// Code to get the current task list in the crontab
-    /// </summary>
-    public static string GetCrontab()
+    public static class Crontab
     {
-        var processStartInfo = new ProcessStartInfo
+        public static string GetCrontab()
         {
-            FileName = "/bin/bash",
-            Arguments = "-c \"crontab -l\"",
-            RedirectStandardOutput = true,
-            UseShellExecute = false
-        };
-
-        var process = new Process
-        {
-            StartInfo = processStartInfo
-        };
-
-        process.Start();
-        string result = process.StandardOutput.ReadToEnd();
-        process.WaitForExit();
-
-        return result;
-    }
-
-    /// <summary>
-    /// Code to open the crontab editor and allow editing tasks
-    /// </summary>
-    public static void EditCrontab()
-    {
-        var processStartInfo = new ProcessStartInfo
-        {
-            FileName = "/bin/bash",
-            Arguments = "-c \"crontab -e\"",
-            UseShellExecute = false
-        };
-
-        var process = new Process
-        {
-            StartInfo = processStartInfo
-        };
-
-        process.Start();
-        process.WaitForExit();
-    }
-
-    /// <summary>
-    /// Code to open the crontab editor and allow adding new tasks
-    /// </summary>
-    public static void AddTask(string task)
-    {
-        var processStartInfo = new ProcessStartInfo
-        {
-            FileName = "/bin/bash",
-            Arguments = $"-c \"echo '{task}' | crontab -e\"",
-            UseShellExecute = false
-        };
-
-        var process = new Process
-        {
-            StartInfo = processStartInfo
-        };
-
-        process.Start();
-        process.WaitForExit();
-    }
-    
-    public static void RemoveTask(string taskIdentifier)
-    {
-        if (string.IsNullOrWhiteSpace(taskIdentifier))
-            throw new ArgumentException("Task identifier cannot be null or empty", nameof(taskIdentifier));
-
-        var processStartInfo = new ProcessStartInfo
-        {
-            FileName = "/bin/bash",
-            Arguments = $"-c \"crontab -l | grep -v '{taskIdentifier}' | crontab -\"",
-            RedirectStandardError = true,
-            UseShellExecute = false
-        };
-
-        try
-        {
-            using (var process = new Process { StartInfo = processStartInfo })
+            try
             {
-                process.Start();
-                string error = process.StandardError.ReadToEnd();
-                process.WaitForExit();
-
-                if (process.ExitCode != 0)
+                var processStartInfo = new ProcessStartInfo
                 {
-                    throw new InvalidOperationException($"Failed to remove task: {error}");
+                    FileName = "/bin/bash",
+                    Arguments = "-c \"crontab -l\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                };
+
+                using (var process = new Process { StartInfo = processStartInfo })
+                {
+                    process.Start();
+                    string result = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+
+                    return result;
                 }
             }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("An error occurred while getting the crontab list", ex);
+            }
         }
-        catch (Exception ex)
+
+        public static void EditCrontab()
         {
-            throw new InvalidOperationException("An error occurred while removing the task from crontab", ex);
+            try
+            {
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = "-c \"crontab -e\"",
+                    UseShellExecute = false
+                };
+
+                using (var process = new Process { StartInfo = processStartInfo })
+                {
+                    process.Start();
+                    process.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("An error occurred while editing the crontab", ex);
+            }
+        }
+
+        public static void AddTask(string task)
+        {
+            if (string.IsNullOrWhiteSpace(task))
+                throw new ArgumentException("Task cannot be null or empty", nameof(task));
+
+            try
+            {
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"echo '{task}' | crontab -e\"",
+                    UseShellExecute = false
+                };
+
+                using (var process = new Process { StartInfo = processStartInfo })
+                {
+                    process.Start();
+                    process.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("An error occurred while adding the task to crontab", ex);
+            }
+        }
+
+        public static void RemoveTask(string taskIdentifier)
+        {
+            if (string.IsNullOrWhiteSpace(taskIdentifier))
+                throw new ArgumentException("Task identifier cannot be null or empty", nameof(taskIdentifier));
+
+            try
+            {
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"crontab -l | grep -v '{taskIdentifier}' | crontab -\"",
+                    RedirectStandardError = true,
+                    UseShellExecute = false
+                };
+
+                using (var process = new Process { StartInfo = processStartInfo })
+                {
+                    process.Start();
+                    string error = process.StandardError.ReadToEnd();
+                    process.WaitForExit();
+
+                    if (process.ExitCode != 0)
+                    {
+                        throw new InvalidOperationException($"Failed to remove task: {error}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("An error occurred while removing the task from crontab", ex);
+            }
+        }
+
+        public static void EnableDisableTask(string taskIdentifier, bool enable)
+        {
+            if (string.IsNullOrWhiteSpace(taskIdentifier))
+                throw new ArgumentException("Task identifier cannot be null or empty", nameof(taskIdentifier));
+
+            try
+            {
+                string command = enable ? $"crontab -l | sed '/# {taskIdentifier}/ s/^# //g' | crontab -" :
+                    $"crontab -l | sed '/{taskIdentifier}/ s/^/# /g' | crontab -";
+
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{command}\"",
+                    UseShellExecute = false
+                };
+
+                using (var process = new Process { StartInfo = processStartInfo })
+                {
+                    process.Start();
+                    process.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("An error occurred while enabling/disabling the task in crontab", ex);
+            }
+        }
+
+        public static string ListTasksWithDetails()
+        {
+            try
+            {
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = "-c \"crontab -l | while read line; do next_run=$(echo $line | awk '{print $1,$2,$3,$4,$5}' | crontab -n); echo $line - Next Run: $next_run; done\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                };
+
+                using (var process = new Process { StartInfo = processStartInfo })
+                {
+                    process.Start();
+                    string result = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("An error occurred while listing tasks with details from crontab", ex);
+            }
+        }
+
+        public static void ExportTasks(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+                throw new ArgumentException("File path cannot be null or empty", nameof(filePath));
+
+            try
+            {
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"crontab -l > {filePath}\"",
+                    UseShellExecute = false
+                };
+
+                using (var process = new Process { StartInfo = processStartInfo })
+                {
+                    process.Start();
+                    process.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("An error occurred while exporting tasks from crontab", ex);
+            }
+        }
+
+        public static void ImportTasks(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+                throw new ArgumentException("File path cannot be null or empty", nameof(filePath));
+
+            try
+            {
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"crontab {filePath}\"",
+                    UseShellExecute = false
+                };
+
+                using (var process = new Process { StartInfo = processStartInfo })
+                {
+                    process.Start();
+                    process.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("An error occurred while importing tasks to crontab", ex);
+            }
         }
     }
 }
